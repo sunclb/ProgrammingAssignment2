ae6efd1454ce64b6cb9487b7fcefdf082a2d146a
SHY <sunclb.sun@gmail.com>
2017-07-20 00:33:26 +0800

    Add comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function provide functions to get, set matrix and get and set the inverse 
+## of the matrix
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
   iv <- NULL
+  ## Set matrix function
   set <- function(y) {
     x <<- y
     iv <<- NULL
   }
+  ## get matrix function
   get <- function() x
+  ## set the inverse of the matrix function
   setinverse <- function(inverse) iv <<- inverse
+  ## get the inverse of the matrix function
   getinverse <- function() iv
+  ## return functions
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -19,15 +24,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function check whether the inverse of matrix is avaliable, if so, get and
+## return it, and if not, calculate the inverse, store it and return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Check whether the inverse of matrix is avaliable. If so, return the inverse
   iv <- x$getinverse()
   if(!is.null(iv)) {
     message("getting cached data")
     return(iv)
   }
+  ## When inverse of matrix is not avaliable, calculate the inverse, store it and return it
   data <- x$get()
   iv <- solve(data)
   x$setinverse(iv)
